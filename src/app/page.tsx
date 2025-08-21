fe83528604f55a3812a02a038722af97fa0b91ec
collaborators <deepakantinoob@gmail.com>
2025-08-21 15:50:13 +0530

    pushing with new branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <p>changes by Collaborator </p>
-            <p>changes by Collaborator  again with rule</p>
+            <p>changes by Collaborator  2</p>
     </div>
   );
 }
