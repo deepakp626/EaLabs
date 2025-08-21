53d330a1c2ad14bf909a7c379d1445049e710409
deepakANT <deepakantinoob@gmail.com>
2025-08-21 17:41:40 +0530

    Update page.tsx
    changes by collaberator github UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <p>changes by Collaborator </p>
             <p>changes by Collaborator  2</p>
+           <p>changes by Collaborator  github UI</p>
     </div>
   );
 }
